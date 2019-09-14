753d0b6c2ae5bddf952198b63ee437a6c0b67c06
Andrew Davis <andrew@deviouscreation.com>
2019-09-14 20:18:31 +0100

    More fixes to powershell script

--- build/sonar-props-builder.ps1
+++ build/sonar-props-builder.ps1
@@ -13,9 +13,9 @@ if ($prId) {
     Add-Content ./sonar.properties "sonar.pullrequest.github.repository=$env:REPO_NAME_VAR"
 } else {
     $isDefaultBranch = true;
-    $currentBranch = $env:BUILD.SOURCEBRANCH
+    $currentBranch = $env:BUILD_SOURCEBRANCH
     $isDefaultBranch = $currentBranch -eq 'refs/heads/master';
-    if (!isDefaultBranch) {
+    if (isDefaultBranch -ne true) {
       # // VSTS-165 don't use Build.SourceBranchName
       Add-Content ./sonar.properties "sonar.branch.name=".BranchName($env:BUILD_SOURCEBRANCH)
     }
@@ -23,7 +23,7 @@ if ($prId) {
 
 function BranchName {
  param( [string]$fullName )
- if ($fullName.StartsWith("refs/heads/")) {
+ if ($fullName.StartsWith("refs/heads/") -eq true) {
     return $fullName.Substring(11);
   }
   return $fullName;
