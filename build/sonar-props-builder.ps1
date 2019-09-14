8d8ee18478816a5866f9c154dca614ec6ca5e01b
Andrew Davis <andrew@deviouscreation.com>
2019-09-14 20:23:10 +0100

    more powershell

--- build/sonar-props-builder.ps1
+++ build/sonar-props-builder.ps1
@@ -12,10 +12,10 @@ if ($prId) {
     Add-Content ./sonar.properties "sonar.pullrequest.provider=github"
     Add-Content ./sonar.properties "sonar.pullrequest.github.repository=$env:REPO_NAME_VAR"
 } else {
-    $isDefaultBranch = true;
+    $isDefaultBranch = $TRUE;
     $currentBranch = $env:BUILD_SOURCEBRANCH
     $isDefaultBranch = $currentBranch -eq 'refs/heads/master';
-    if (isDefaultBranch -ne true) {
+    if (isDefaultBranch -ne $TRUE) {
       # // VSTS-165 don't use Build.SourceBranchName
       Add-Content ./sonar.properties "sonar.branch.name=".BranchName($env:BUILD_SOURCEBRANCH)
     }
@@ -23,7 +23,7 @@ if ($prId) {
 
 function BranchName {
  param( [string]$fullName )
- if ($fullName.StartsWith("refs/heads/") -eq true) {
+ if ($fullName.StartsWith("refs/heads/") -eq $TRUE) {
     return $fullName.Substring(11);
   }
   return $fullName;
