9c86a1f5e40fe2a8dcc36c3c3463f9861f7590cd
Andrew Davis <andrew@deviouscreation.com>
2019-09-16 14:55:03 +0100

    Updating the build process to generate build metadata

---
+++ build/artifact-builder.ps1
@@ -0,0 +1,21 @@
+function Add-Property {
+    param(
+        [System.Xml.XmlDocument] $doc,
+        [string] $name,
+        [string] $value
+    )
+    
+    $child = $doc.CreateElement("Property", $doc.DocumentElement.NamespaceURI)
+    $child.SetAttribute("Name", $name)
+    $child.InnerText = $value
+    $doc.DocumentElement.AppendChild($child)
+}
+
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load("./Build.Artifacts.temp.xml")
+
+Add-Property -doc $doc -name "build.version" -value $env:BUILD_BUILDNUMBER
+Add-Property -doc $doc -name "github.commitSha" -value $env:GITVERSION_SHA
+Add-Property -doc $doc -name "github.commitShortSha" -value $env:GITVERSION_SHORTSHA
+
+$doc.Save("./Build.Artifacts.xml");
